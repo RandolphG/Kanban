dcc4b558de3dd318ac61e8807a0cf621a0f02fae
Randolph Gordon <randolph.gordon@poplogics.com>
2021-04-22 12:13:07 +0100

    first commit

---
+++ src/store/reducers/boardOrderReducer.js
@@ -0,0 +1,17 @@
+import { CONSTANTS } from "../../actions";
+
+const initialState = ["board-0"];
+
+const boardOrderReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CONSTANTS.ADD_BOARD: {
+      console.log(`added board-${action.payload.id} `);
+
+      return [...state, `board-${action.payload.id}`];
+    }
+    default:
+      return state;
+  }
+};
+
+export default boardOrderReducer;
